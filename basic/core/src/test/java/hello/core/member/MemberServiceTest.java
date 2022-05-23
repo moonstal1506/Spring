2d4f35bfbc55d85a4e6090742a0b5cafc956aba2
moonstal1506 <moonstal1506@gmail.com>
2022-05-23 11:44:51 +0900

    주문과 할인 도메인

--- basic/core/src/test/java/hello/core/member/MemberServiceTest.java
+++ basic/core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class MemberServiceTest {
-    
+
     MemberService memberService = new MemberServiceImpl();
 
     @Test
