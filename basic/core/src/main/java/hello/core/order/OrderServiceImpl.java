e47300ab6fd11c4f739cb4ddd6d365bd5f94fd63
moonstal1506 <moonstal1506@gmail.com>
2022-05-30 13:16:48 +0900

    중복 등록과 충돌

--- basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,12 +1,11 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
-import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
