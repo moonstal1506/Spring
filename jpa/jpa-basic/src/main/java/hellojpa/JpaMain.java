c5e5efb172a361ed270cead526f9c784585c9e59
moonstal1506 <moonstal1506@gmail.com>
2022-06-21 13:18:18 +0900

    엔티티 매핑

--- jpa/jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ jpa/jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -22,13 +22,29 @@ public class JpaMain {
 //            Member findMember = em.find(Member.class, 1L);
 //            findMember.setName("aaa");
 
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(1)
-                    .setMaxResults(10)
-                    .getResultList();
-            for (Member member : result) {
-                System.out.println("member.getName() = " + member.getName());
-            }
+//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+//                    .setFirstResult(1)
+//                    .setMaxResults(10)
+//                    .getResultList();
+//            for (Member member : result) {
+//                System.out.println("member.getName() = " + member.getName());
+//            }
+
+//            Member a = new Member(11L, "a");
+//            Member b = new Member(22L, "b");
+//
+//            em.persist(a);
+//            em.persist(b);
+
+//            Member findMember = em.find(Member.class, 11L);
+//            findMember.setName("aaa");
+//            System.out.println("----------------");
+
+            Member member = new Member();
+            member.setId(1L);
+            member.setUsername("a");
+            member.setRoleType(RoleType.USER);
+            em.persist(member);
 
             tx.commit();
         }catch (Exception e){
