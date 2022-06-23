baa5ed6379e4187d3ea03d9115661e335e7b7ab0
moonstal1506 <moonstal1506@gmail.com>
2022-06-23 14:05:22 +0900

    즉시로딩과 지연로딩

--- jpa/jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ jpa/jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -16,40 +16,27 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-//            Member member = em.find(Member.class, 1L);
-//            printMember(member);
-//            printMemberAndTeam(member);
+
+            Team team = new Team();
+            team.setName("team");
+            em.persist(team);
 
             Member member = new Member();
             member.setUsername("aa");
+            member.setTeam(team);
             em.persist(member);
-
-
-//            Member member2 = new Member();
-//            member2.setUsername("bb");
-
-//            em.persist(member2);
 
             em.flush();
             em.clear();
 
-//            Member m1 = em.find(Member.class, member.getId());
-            Member Reference = em.getReference(Member.class, member.getId());//프록시
-            Reference.getUsername();
-            emf.getPersistenceUnitUtil().isLoaded(Reference);//초기화 여부확인
+//            Member m = em.find(Member.class, member.getId());
+//            m.getTeam().getClass();//프록시
+//            m.getTeam().getName();//이때 초기화(지연로딩)
 
-            //준영속 상태
-//            em.detach(Reference);
-//            em.close();
-//            em.clear();
-            Hibernate.initialize((Reference));//강제초기화
-            Reference.getUsername();//초기화
-//            System.out.println(m1==Reference);//항상같아야함
-//            Member m2 = em.getReference(Member.class, member2.getId());//프록시
-
-//            System.out.println("findMember = " + findMember.getUsername());
-
-//            System.out.println("m1 = m2: " + (m1.getClass()==m2.getClass()));다름
+            //sql: select * from member
+            //sql: select *from Team where TEAM_ID=xxx;
+            List<Member> members = em.createQuery("select m from Member m join fetch m.team", Member.class)
+                    .getResultList();
 
             tx.commit();
         }catch (Exception e){
