b35722f20c0d2933ca1a5572d99a2d4b72882b38
moonstal1506 <moonstal1506@gmail.com>
2022-06-24 15:44:00 +0900

    실전-값타입 매핑

--- jpa/jpabook/src/main/java/jpabook/jpashop/domain/Delivery.java
+++ jpa/jpabook/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -8,12 +8,43 @@ public class Delivery extends BaseEntity{
     @Id @GeneratedValue
     private Long id;
 
-    private String city;
-    private String street;
-    private String zipcode;
+    @Embedded
+    private Address address;
 
     @OneToOne(mappedBy = "delivery" ,fetch = FetchType.LAZY)
     private Order order;
 
     private DeliveryStatus status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public DeliveryStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(DeliveryStatus status) {
+        this.status = status;
+    }
 }
