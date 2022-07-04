698269252b4e5203148631aef90e390a54a4174c
moonstal1506 <moonstal1506@gmail.com>
2022-07-04 09:10:09 +0900

    NamedQuery

--- jpa/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ jpa/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,13 +1,18 @@
 package study.datajpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import study.datajpa.entity.Member;
 
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    List<Member> findByUsernameAndAgeGreaterThan(String username,int age);
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 
     List<Member> findTop3HelloBy();
+
+//    @Query(name = "Member.findByUsername")
+    List<Member> findByUsername(@Param("username") String username);
 }
