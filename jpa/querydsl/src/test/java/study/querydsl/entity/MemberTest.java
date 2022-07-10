6fc182f87e57fcac5b1cd0221aca157cf170d24c
moonstal1506 <moonstal1506@gmail.com>
2022-07-10 13:45:15 +0900

    예제 도메인

---
+++ jpa/querydsl/src/test/java/study/querydsl/entity/MemberTest.java
@@ -0,0 +1,51 @@
+package study.querydsl.entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    void testEntity() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member mem1 = new Member("mem1", 10, teamA);
+        Member mem2 = new Member("mem2", 20, teamA);
+        Member mem3 = new Member("mem3", 30, teamB);
+        Member mem4 = new Member("mem4", 40, teamB);
+
+        em.persist(mem1);
+        em.persist(mem2);
+        em.persist(mem3);
+        em.persist(mem4);
+
+        //초기화
+        em.flush();
+        em.clear();
+
+        List<Member> members = em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+
+        for (Member member : members) {
+            System.out.println("member = " + member);
+            System.out.println("member.getTeam() = " + member.getTeam());
+        }
+    }
+}
