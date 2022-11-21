d5e8ef64b4a0d3eedfa1ef0bbeff8bf27b24459e
moonstal1506 <moonstal1506@gmail.com>
2022-11-21 20:07:49 +0900

    dynamicB

--- proxy/src/test/java/hello/proxy/jdkdynamic/JdkDynamicProxyTest.java
+++ proxy/src/test/java/hello/proxy/jdkdynamic/JdkDynamicProxyTest.java
@@ -1,8 +1,6 @@
 package hello.proxy.jdkdynamic;
 
-import hello.proxy.jdkdynamic.code.AImpl;
-import hello.proxy.jdkdynamic.code.AInterface;
-import hello.proxy.jdkdynamic.code.TimeInvocationHandler;
+import hello.proxy.jdkdynamic.code.*;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -17,6 +15,18 @@ public class JdkDynamicProxyTest {
         TimeInvocationHandler handler = new TimeInvocationHandler(target);
 
         AInterface proxy = (AInterface) Proxy.newProxyInstance(AInterface.class.getClassLoader(), new Class[]{AInterface.class}, handler);
+
+        proxy.call();
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+    }
+
+    @Test
+    void dynamicB() {
+        BInterface target = new BImpl();
+        TimeInvocationHandler handler = new TimeInvocationHandler(target);
+
+        BInterface proxy = (BInterface) Proxy.newProxyInstance(BInterface.class.getClassLoader(), new Class[]{BInterface.class}, handler);
 
         proxy.call();
         log.info("targetClass={}", target.getClass());
