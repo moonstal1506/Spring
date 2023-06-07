3c90a05dcb5106c6e6ce9f6e68ec5c5bd606ed40
moonstal1506 <moonstal1506@gmail.com>
2023-06-07 11:54:14 +0900

    외부 설정 - 커맨드 라인 옵션 인수와 스프링 부트

--- springboot/external/src/main/java/hello/ExternalApplication.java
+++ springboot/external/src/main/java/hello/ExternalApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ExternalApplication {
 
+    //--url=devdb --username=dev_user --password=dev_pw mode=on
     public static void main(String[] args) {
         SpringApplication.run(ExternalApplication.class, args);
     }
