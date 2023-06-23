b5413db8bf9c6ec6580ecae0d5fb74bab95d5153
moonstal1506 <moonstal1506@gmail.com>
2023-06-23 12:33:59 +0900

    메트릭 등록2 - @Counted

--- springboot/actuator/src/main/java/hello/ActuatorApplication.java
+++ springboot/actuator/src/main/java/hello/ActuatorApplication.java
@@ -1,16 +1,15 @@
 package hello;
 
-import hello.order.v0.OrderConfigV0;
-import hello.order.v1.OrderConfigV1;
+import hello.order.v2.OrderConfigV2;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.audit.InMemoryAuditEventRepository;
 import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 //@Import(OrderConfigV0.class)
-@Import(OrderConfigV1.class)
+//@Import(OrderConfigV1.class)
+@Import(OrderConfigV2.class)
 @SpringBootApplication(scanBasePackages = "hello.controller")
 public class ActuatorApplication {
 
